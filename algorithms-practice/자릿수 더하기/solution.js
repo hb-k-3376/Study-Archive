6258d4fc089c0ac3762043fd636c0c3140676492
Jaden <hyunbin.kim.3376@gmail.com>
2025-04-23 20:09:31 +0900

    홀수와짝수

--- algorithms-practice/자릿수 더하기/solution.js
+++ algorithms-practice/자릿수 더하기/solution.js
@@ -1,9 +1,5 @@
 export {};
 
-function solution(n) {
-  return String(n).split('').reverse().map(Number);
+function solution(num) {
+  return num % 2 === 0 ? 'Even' : 'Odd';
 }
-
-/**
- * 먼저 n을 문자열로 변환 후 배열로 reverse() 로 뒤집고 문자열로 변환
- */
