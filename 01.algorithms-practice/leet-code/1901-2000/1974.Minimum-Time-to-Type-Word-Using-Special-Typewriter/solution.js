a7d3f91c921969404aced32fcc427a5093864d5d
Jaden <hyunbin.kim.3376@gmail.com>
2025-05-31 22:27:48 +0900

    leet code 1974번 문제 및 풀이

--- 01.algorithms-practice/leet-code/1901-2000/1974.Minimum-Time-to-Type-Word-Using-Special-Typewriter/solution.js
+++ 01.algorithms-practice/leet-code/1901-2000/1974.Minimum-Time-to-Type-Word-Using-Special-Typewriter/solution.js
@@ -2,4 +2,27 @@
  * @param {string} word
  * @return {number}
  */
-var minTimeToType = function (word) {};
+var minTimeToType = function (word) {
+  let step = 0; // 이동 거리
+  let pointer = 'a'.charCodeAt(); // 초기 포인터
+
+  for (const c of word) {
+    const target = c.charCodeAt(); // 이동할 자판
+    // 타겟과 현재 위치가 같을 경우 넘어감
+    if (pointer === target) continue;
+    const dist = Math.abs(target - pointer); // 이동할 거리 측정
+
+    if (dist > 13) {
+      // 이동할 거리가 13보다 멀면 시계 반대 방향으로 이동
+      step += 26 - dist;
+    }
+    // 이동할 거리가 13과 같거나 작은 경우는 시계 방향으로 이동
+    else {
+      step += dist;
+    }
+
+    pointer = target; // 포인터 이동
+  }
+
+  return step + word.length;
+};
