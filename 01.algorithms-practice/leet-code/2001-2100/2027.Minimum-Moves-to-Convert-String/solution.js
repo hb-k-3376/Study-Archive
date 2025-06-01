38d630bb20322a7ade7f62b55422971cec8ba16b
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-01 14:04:07 +0900

    leet code 2027번 문제 및 풀이

--- 01.algorithms-practice/leet-code/2001-2100/2027.Minimum-Moves-to-Convert-String/solution.js
+++ 01.algorithms-practice/leet-code/2001-2100/2027.Minimum-Moves-to-Convert-String/solution.js
@@ -1,39 +1,20 @@
 /**
- * @param {Function} fn
- * @return {Function}
+ * @param {string} s
+ * @return {number}
  */
-function memoize(fn) {
-  // 계산 된 값을 저장하는 객체
-  const cache = {};
-  let count = 0; // 연산 카운트
+var minimumMoves = function (s) {
+  let step = 0;
 
-  return function (...args) {
-    // 전달 받은 파라미터를 키로 사용하기 위해 string으로 변환
-    // map의 키 값으로 사용해야한다.
-    const key = JSON.stringify(args);
-    // cache에 키가 있는지 없는지 확인
-    // 있다면 결과를 바로 리턴
-    if (key in cache) {
-      return cache[key];
+  for (let i = 0; i < s.length; ) {
+    if (s[i] === 'X') {
+      // x를 만나게 되면
+      i += 3; // 3칸 이동
+      step++; // step 카운트
     }
-    // 연산 카운트 업
-    count++;
-    // 연산 apply(this, args) 를 이용해서 호출하는 이유는
-    // fn이 객체 메소드이고 객체 안에서 요소를 참조하는 경우 this값을 전달하기 위해서
-    const result = fn.apply(this, args);
-    cache[key] = result;
-
-    return result;
-  };
-}
-
-/**
- * let callCount = 0;
- * const memoizedFn = memoize(function (a, b) {
- *	 callCount += 1;
- *   return a + b;
- * })
- * memoizedFn(2, 3) // 5
- * memoizedFn(2, 3) // 5
- * console.log(callCount) // 1
- */
+    // O번 페스
+    else {
+      i++;
+    }
+  }
+  return step;
+};
