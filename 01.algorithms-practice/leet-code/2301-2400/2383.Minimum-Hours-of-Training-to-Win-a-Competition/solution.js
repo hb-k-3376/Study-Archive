b09ee3999e33a135a4c54b82d4c0a067adea8d84
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-05 09:53:06 +0900

    leet code 2566번 문제 및 풀이

---
+++ 01.algorithms-practice/leet-code/2301-2400/2383.Minimum-Hours-of-Training-to-Win-a-Competition/solution.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} initialEnergy
+ * @param {number} initialExperience
+ * @param {number[]} energy
+ * @param {number[]} experience
+ * @return {number}
+ */
+var minNumberOfHours = function (initialEnergy, initialExperience, energy, experience) {};
--- 01.algorithms-practice/leet-code/2501-2600/2566.Maximum-Difference-by-Remapping-a-Digit/solution.js
+++ 01.algorithms-practice/leet-code/2501-2600/2566.Maximum-Difference-by-Remapping-a-Digit/solution.js
@@ -4,28 +4,17 @@
  */
 var minMaxDifference = function (num) {
   const numStr = String(num);
-  let maxDigit = null;
-  let minDigit = null;
 
-  // 반복문을 돌려서 앞의 자릿수가 9가인 숫자를 찾는다.
-  for (let i = 0; i < numStr.length; i++) {
-    if (numStr[i] !== '9') {
-      maxDigit = numStr[i];
+  // 9가 아닌 가장 앞에있는 숫자를 찾은 다음 9변경하여 최댓값을 찾음
+  max = numStr;
+  for (const c of numStr) {
+    if (c !== '9') {
+      max = numStr.replaceAll(c, '9');
       break;
     }
   }
-  // 가장 큰 숫자
-  const max = maxDigit ? numStr.replaceAll(maxDigit, '9') : numStr;
+  // 가장 앞에 숫자를 0으로 항상 변경하는게 최솟값을 구하는 방법임.
+  min = numStr.replaceAll(numStr[0], '0');
 
-  // 반복문을 돌려서 앞의 자릿수가 0가인 숫자를 찾는다.
-  for (let i = 0; i < numStr.length; i++) {
-    if (numStr[i] !== '0') {
-      minDigit = numStr[i];
-      break;
-    }
-  }
-  // 가장 작은 숫자
-  const min = minDigit ? numStr.replaceAll(minDigit, '0') : numStr;
-
-  return +max - +min;
+  return +max - +max;
 };
