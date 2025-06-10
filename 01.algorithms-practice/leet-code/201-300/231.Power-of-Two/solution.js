f19e30b3b0905f0cee055026be8c44612a8919fe
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-10 20:30:02 +0900

    leet code 231번 풀이 업데이트

--- 01.algorithms-practice/leet-code/201-300/231.Power-of-Two/solution.js
+++ 01.algorithms-practice/leet-code/201-300/231.Power-of-Two/solution.js
@@ -6,14 +6,9 @@ var isPowerOfTwo = function (n) {
   // 0이거나 음수 인 경우 false 반환
   if (n <= 0) return false;
 
-  // n이 1 보다 큰 경우에만 반복문을 돌린다.
-  while (n > 1) {
-    // n을 2로 나눴을떼 나머지가 발생한다면 2의 거듭 제곱이 아니다.
-    if (n % 2 !== 0) {
-      return false;
-    }
-    // 2로 나누며 n의 값 업데이트
+  // 2 계속 나누다가 홀수가 되면 반복문 끝냄
+  while (n % 2 === 0) {
     n /= 2;
   }
-  return true;
+  return n === 1;
 };
