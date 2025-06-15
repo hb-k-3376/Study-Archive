550c47c2aac45352c6174dba32b98dbb5b13b02c
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-16 07:38:24 +0900

    leet code 441번 문제 및 풀이

--- 01.algorithms-practice/leet-code/401-500/441.Arranging-Coins/solution.js
+++ 01.algorithms-practice/leet-code/401-500/441.Arranging-Coins/solution.js
@@ -3,10 +3,15 @@
  * @return {number}
  */
 var arrangeCoins = function (n) {
+  let coins = n;
   for (let i = 0; i < n; i++) {
-    n = -i + 1;
-    if (n < 0) {
+    coins -= i + 1;
+
+    if (coins === 0) {
       return i + 1;
+    }
+    if (coins < 0) {
+      return i;
     }
   }
 };
