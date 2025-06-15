413cdb2aba870ecd18657ac53a93dc662e5c2333
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-15 17:17:23 +0900

    leet code 389번 풀이 업데이트

--- 01.algorithms-practice/leet-code/301-400/389.Find-the-Difference/solution.js
+++ 01.algorithms-practice/leet-code/301-400/389.Find-the-Difference/solution.js
@@ -11,10 +11,10 @@ var findTheDifference = function (s, t) {
   }
 
   for (let i = 0; i < t.length; i++) {
-    if (map.get(t[i])) map.set(t[i], map.get(t[i]) - 1);
-
     if (!map.has(t[i]) || map.get(t[i]) < 1) {
       return t[i];
     }
+
+    map.set(t[i], map.get(t[i]) - 1);
   }
 };
