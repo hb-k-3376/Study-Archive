7b37be56a8717bb3a59fc25bb0df255dc0c63934
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-19 16:19:56 +0900

    leet code 506번 문제 및 풀이

--- 01.algorithms-practice/leet-code/501-600/506.Relative-Ranks/solution.js
+++ 01.algorithms-practice/leet-code/501-600/506.Relative-Ranks/solution.js
@@ -2,4 +2,19 @@
  * @param {number[]} score
  * @return {string[]}
  */
-var findRelativeRanks = function (score) {};
+var findRelativeRanks = function (score) {
+  // 메달도 배열로 만듦
+  const medals = ['Gold Medal', 'Silver Medal', 'Bronze Medal'];
+  // 정렬된 배열 새로 만듬
+  const sortedScore = score.toSorted((a, b) => b - a);
+
+  const map = new Map();
+
+  for (let i = 0; i < sortedScore.length; i++) {
+    const score = sortedScore[i];
+    // 0 - 2 인 값들은 메달을 순차적으로 설정해줌
+    map.set(score, i < 3 ? medals[i] : String(i + 1));
+  }
+
+  return score.map((point) => map.get(point));
+};
