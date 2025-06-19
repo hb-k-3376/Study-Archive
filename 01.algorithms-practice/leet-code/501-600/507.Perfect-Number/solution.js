2c1f1823066e2c3bbfd124fc323047ff6e2a0b8c
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-20 08:19:18 +0900

    leet code 507번 문제 및 풀이

--- 01.algorithms-practice/leet-code/501-600/507.Perfect-Number/solution.js
+++ 01.algorithms-practice/leet-code/501-600/507.Perfect-Number/solution.js
@@ -3,16 +3,18 @@
  * @return {boolean}
  */
 var checkPerfectNumber = function (num) {
-  const dividers = [];
+  if (num === 1) return false;
+  let sum = 1; // 1은 모든 수의 약수
 
-  for (let i = 1; i <= Math.sqrt(num); i++) {
+  // 2 부터 시작하여 자기 자신은 포함이 안됨
+  for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
-      dividers.push(i);
+      sum += i;
       if (i !== num / i) {
-        dividers.push(num / i);
+        sum += num / i;
       }
     }
   }
 
-  return dividers.reduce((sum, num) => sum + num, 0) - num === num;
+  return sum === num;
 };
