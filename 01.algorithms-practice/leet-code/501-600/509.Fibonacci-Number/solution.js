66de9716036f8618b41bc447fa8e8b59e0ad68fa
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-20 19:56:24 +0900

    leet code 509번 문제 및 풀이

--- 01.algorithms-practice/leet-code/501-600/509.Fibonacci-Number/solution.js
+++ 01.algorithms-practice/leet-code/501-600/509.Fibonacci-Number/solution.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var fib = function (n) {
-  const map = {};
+  const cache = {};
 
   const f = (n) => {
     if (n === 0) return 0;
     if (n === 1) return 1;
 
-    if (map[n]) {
-      return map[n];
+    if (cache[n]) {
+      return cache[n];
     }
-    map[n] = f(n - 1) + f(n - 2);
-    return map[n];
+    cache[n] = f(n - 1) + f(n - 2);
+    return cache[n];
   };
 
   return f(n);
